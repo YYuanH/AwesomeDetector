1e3c07b530bc6697c67cdfaccf59a1e7bf50bc88
ZionQAQ <zique@foxmail.com>
2020-05-27 16:37:54 +0800

    Myline update

--- src/component/myline/index.js
+++ src/component/myline/index.js
@@ -113,7 +113,7 @@ export default class MyLine extends Component {
       case `timestamp`: return '时间戳'
       case `loss_rate`: return '丢包率'
       case `routers`: return '跳数'
-      case 'value': return `客户端 ${this.props.clientId} 传输速度`
+      case 'value': return !!this.props.extraData ? `客户端 ${this.props.clientId} 传输时延` : `客户端 ${this.props.clientId} 传输速度`
       case 'ping_speed': return `客户端 ${this.props.clientId} 传输时延`
       default: return name
     }
@@ -163,20 +163,6 @@ export default class MyLine extends Component {
         start: 0,
         end: 100
       }, 
-      // {
-      //   start: 0,
-      //   end: 50,
-      //   bottom: 0,
-      //   handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
-      //   handleSize: '90%',
-      //   handleStyle: {
-      //     color: '#fff',
-      //     shadowBlur: 3,
-      //     shadowColor: 'rgba(0, 0, 0, 0.6)',
-      //     shadowOffsetX: 2,
-      //     shadowOffsetY: 2
-      //   }
-      // }
     ],
       series: this.props.data ? Object.keys(this.props.data).map((item, index) => {
         if(item !== 'timestamp') {
@@ -196,7 +182,7 @@ export default class MyLine extends Component {
                 shadowBlur: 10,
               }
             },
-            data: item == 'ping_speed' ? this.props.data[item].map(item => parseInt(item)) : this.props.data[item].map(item => (item/1000/1000).toFixed(3))
+            data: !!this.props.extraData ? this.props.data[item].map(item => parseInt(item)) : this.props.data[item].map(item => (item/1000/1000).toFixed(3))
           };
         }
         return null;
