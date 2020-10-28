f7bdb89b8fbc4c69fc24f555bbdef494b3d70124
YYuanH <1079312238@qq.com>
2020-10-28 16:52:07 +0800

    修复测速页面折线图横坐标时间显示Bug

--- src/utils/timeFormat.js
+++ src/utils/timeFormat.js
@@ -4,7 +4,7 @@ export default function timeFormat(timeArray) {
             let M = (dateObj.getMonth() + 1 < 10 ? '0' + (dateObj.getMonth() + 1) : dateObj.getMonth() + 1) + '-';
             let D = dateObj.getDate() + ' ';
             let h = (dateObj.getHours() < 10 ? '0' + dateObj.getHours() : dateObj.getHours()) + ':';
-            let m = dateObj.getMinutes() < 10 ? '0' + dateObj.getMinutes() : dateObj.getMinutes();
+            let m = (dateObj.getMinutes() < 10 ? '0' + dateObj.getMinutes() : dateObj.getMinutes()) + ':';
             let s = dateObj.getSeconds();
             let time = M + D + h + m + s;
             return time;
